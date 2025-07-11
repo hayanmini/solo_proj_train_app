635a4f3384f37256c3fb4b3981b330fd7a8f30a5
hayanmini <dudals9696@gmail.com>
2025-07-11 20:12:33 +0900

    UI 디자인 완료, 기능 구현
    - seat 페이지 UI 디자인 완료
    - seat 예매하기 버튼 메세지 출력 완료
    - seat 사용자 입력 받기 구현중

--- lib/pages/seat/widgets/book_now_botton.dart
+++ lib/pages/seat/widgets/book_now_botton.dart
@@ -1,30 +1,76 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_train_app/pages/home/home_page.dart';
 
 class BookNowBotton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 50,
-      child: ElevatedButton(
-        // 버튼 상호작용
-        onPressed: () {},
-        // 버튼 디자인
-        style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.purple,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            )),
-        // 텍스트 디자인
-        child: const Text(
-          "예매 하기",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+    return GestureDetector(
+      child: SizedBox(
+        width: double.infinity,
+        height: 50,
+        child: ElevatedButton(
+          // 버튼 상호작용
+          onPressed: () {
+            showCupertinoDialog(
+              context: context,
+              builder: (context) {
+                return CupertinoAlertDialog(
+                  title: const Text("예약 하시겠습니까?"),
+                  content: Text("좌석 : {$rowName}-{$colNum}"),
+                  actions: [
+                    cancelBotton(context),
+                    checkBotton(context),
+                  ],
+                );
+              },
+            );
+          },
+
+          // 버튼 디자인
+          style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.purple,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              )),
+
+          // 텍스트 디자인
+          child: const Text(
+            "예매 하기",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  // 확인 버튼
+  Widget checkBotton(BuildContext context) {
+    return CupertinoDialogAction(
+      isDefaultAction: true,
+      onPressed: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HomePage(),
+            ));
+      },
+      child: const Text("확인"),
+    );
+  }
+
+  // 취소 버튼
+  Widget cancelBotton(BuildContext context) {
+    return CupertinoDialogAction(
+      isDestructiveAction: true,
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+      child: const Text("취소"),
     );
   }
 }
--- lib/pages/seat/widgets/seat_box.dart
+++ lib/pages/seat/widgets/seat_box.dart
@@ -1,45 +1,58 @@
 import 'package:flutter/material.dart';
 
 class SeatBox extends StatelessWidget {
+  SeatBox(this.selectedRow, this.selectedCol, this.onSelected);
+  String? selectedRow;
+  int? selectedCol;
+  void Function(String row, int col) onSelected;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         seatABC(),
+        seat(1),
+        seat(2),
+        seat(3),
+        seat(4),
+        seat(5),
+        seat(6),
+        seat(7),
+        seat(8),
+        seat(9),
+        seat(10),
         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            seatBox(false),
-            seatBox(false),
-            Text("1"),
-            seatBox(false),
-            seatBox(false),
+            lable("Abailable", Colors.grey[300]!),
+            lable("Selected", Colors.purple)
           ],
-        ),
+        )
       ],
     );
   }
 
   // 좌석 행
-  Row seatABC() {
+  Widget seatABC() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        seatA("A"),
-        seatA("B"),
-        seatA(" "),
-        seatA("C"),
-        seatA("D"),
+        seatAbcd("A"),
+        seatAbcd("B"),
+        seatAbcd(" "),
+        seatAbcd("C"),
+        seatAbcd("D"),
       ],
     );
   }
 
   // 좌석 행 디자인
-  SizedBox seatA(String abc) {
+  Widget seatAbcd(String rowName) {
     return SizedBox.square(
       dimension: 50,
       child: Center(
         child: Text(
-          "$abc",
+          "$rowName",
           style: const TextStyle(
             fontSize: 18,
           ),
@@ -48,14 +61,74 @@ class SeatBox extends StatelessWidget {
     );
   }
 
+  Widget lable(String text, Color color) {
+    return Row(
+      children: [
+        Text("$text"),
+        Container(
+          width: 50,
+          height: 50,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(8),
+          ),
+        )
+      ],
+    );
+  }
+
+  // 좌석 가로 한줄
+  Widget seat(int rowName) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            seatBox("A", rowName),
+            const SizedBox(width: 4),
+            seatBox("B", rowName),
+            numBox("1"),
+            seatBox("C", rowName),
+            const SizedBox(width: 4),
+            seatBox("D", rowName),
+          ],
+        ),
+        const SizedBox(height: 8),
+      ],
+    );
+  }
+
+  Widget numBox(String rowName) {
+    return Column(
+      children: [
+        SizedBox.square(
+          dimension: 50,
+          child: Center(
+            child: Text("$rowName"),
+          ),
+        ),
+      ],
+    );
+  }
+
   // 좌석 박스 디자인
-  Container seatBox(selectSeet) {
-    return Container(
-      width: 50,
-      height: 50,
-      decoration: BoxDecoration(
-        color: selectSeet ? Colors.purple : Colors.grey[300]!,
-        borderRadius: BorderRadius.circular(8),
+  Widget seatBox(String rowName, int colNum) {
+    return GestureDetector(
+      onTap: () {
+        onSelected(rowName, colNum);
+      },
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          width: 50,
+          height: 50,
+          decoration: BoxDecoration(
+            color: rowName == selectedRow && colNum == selectedCol
+                ? Colors.purple
+                : Colors.grey[300]!,
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
       ),
     );
   }
